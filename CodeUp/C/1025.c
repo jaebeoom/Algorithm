d072e69146ffde52481bcb75d48a944ed85a0917
jae-beom <ilovesjdm@gmail.com>
2022-08-18 23:23:42 +0900

    for (int i = 1021; i < 1026; i++)

---
+++ CodeUp/C/1025.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 0, b = 0, c = 0, d = 0, e = 0;
+
+    scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
+    printf("[%d]\n",a*10000);
+    printf("[%d]\n",b*1000);
+    printf("[%d]\n",c*100);
+    printf("[%d]\n",d*10);
+    printf("[%d]\n",e*1);
+
+
+    return 0;
+
+}
