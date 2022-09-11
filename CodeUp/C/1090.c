d90b86921fe5b037c4f5249707fda001d19a1bbc
jae-beom <ilovesjdm@gmail.com>
2022-09-11 22:24:01 +0900

    1091

--- CodeUp/C/1090.c
+++ CodeUp/C/1090.c
@@ -15,3 +15,20 @@ int main() {
     return 0;
 
 }
+#include <stdio.h>
+
+int main() {
+
+    long long int a, b, c = 0;
+    scanf("%lld %lld %lld", &a, &b, &c);
+
+    for(int i = 1; i < c; i++){
+
+        a *= b;
+
+    }
+    printf("%lld", a);
+
+    return 0;
+
+}
