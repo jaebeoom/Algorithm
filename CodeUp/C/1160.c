60a39d80509763bf4de73ddd8a1e572db3c2b1a0
jae-beom <ilovesjdm@gmail.com>
2022-11-30 22:41:15 +0900

    1160

---
+++ CodeUp/C/1160.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int day = 0;
+
+	scanf("%d", &day);
+    
+	if(day % 2 == 1) 
+        printf("oh my god");
+    else 
+        printf("enjoy");
+        
+	return 0;
+}
