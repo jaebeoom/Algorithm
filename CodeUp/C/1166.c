2fcebcf1c2c6cc7c5ff4aeeee24332ba7938d108
jae-beom <ilovesjdm@gmail.com>
2022-12-06 01:05:22 +0900

    1166

---
+++ CodeUp/C/1166.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int year = 0;
+
+    scanf("%d", &year);
+    
+    if(((year % 4 == 0) && (year % 100 != 0)) ||  (year % 400 == 0))
+        printf("Leap");
+    else
+        printf("Normal");              
+        
+	return 0;
+}
