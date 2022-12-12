364ebfa3264b8239661ad2cbaa9b17138a5b4c0e
jae-beom <ilovesjdm@gmail.com>
2022-12-12 10:52:21 +0900

    1172

---
+++ CodeUp/C/1172.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2, num3, temp = 0;
+
+    scanf("%d %d %d", &num1, &num2, &num3);
+    
+     if(num1 >= num2){
+        temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+    if(num2 >= num3){
+        temp = num3;
+        num3 = num2;
+        num2 = temp;
+    }
+    if(num1 >= num2){
+        temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+ 
+    printf("%d %d %d", num1, num2, num3);
+        
+	return 0;
+}
