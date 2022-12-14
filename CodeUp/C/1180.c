319af59bc5af4cde75dd0237ff454cb8384a5ac2
jae-beom <ilovesjdm@gmail.com>
2022-12-14 12:40:27 +0900

    1180

---
+++ CodeUp/C/1180.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	int num, a, b = 0;
+    scanf("%d", &num);
+    
+    a = num / 10;
+    b = num % 10;
+    
+    num = (b * 10 + a) * 2;
+    
+    if(num >= 100) {
+        num = num - 100;
+    }
+
+    printf("%d\n",num);
+    
+    if(num <= 50) {
+        printf("GOOD");
+    }
+    else {
+        printf("OH MY GOD");
+    }
+        
+	return 0;
+}
