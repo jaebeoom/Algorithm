3c5c4b819813439bbd0b2e939302d2be7226aab8
jae-beom <ilovesjdm@gmail.com>
2022-12-26 07:34:44 +0900

    1218

--- CodeUp/C/1216.c
+++ CodeUp/C/1216.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-	int num1 , num2 ,num3 = 0;
-    scanf("%d %d %d",&num1, &num2 , &num3);
+	int num1, num2, num3 = 0;
+    scanf("%d %d %d",&num1, &num2, &num3);
     
     if((num2 - num1) > num3){printf("advertise");}
     else if((num2 - num1) == num3){printf("does not matter");}
