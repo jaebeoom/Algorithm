979cdb91a2b21ac9d0d96cae5fed1942a1f299e9
jae-beom <ilovesjdm@gmail.com>
2022-12-28 22:57:25 +0900

    1224

---
+++ CodeUp/C/1224.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	float num1, num2, num3, num4 = 0.0;
+    scanf("%f %f %f %f",&num1, &num2, &num3, &num4);
+    
+    float a,b = 0.0;
+    
+    a = (float)(num1 / num2);
+    b = (float)(num3 / num4);
+    
+    if(a > b){printf(">");}
+    else if(a == b){printf("=");}
+    else{printf("<");}
+    
+    
+	return 0;
+}
